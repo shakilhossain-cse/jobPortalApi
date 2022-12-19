a8778705a399cd9631668d5572ddbaa9d09d407b
shakil hossain <shakilhossainp.cse@gmail.com>
2022-12-19 23:14:55 +0600

    added login and logout

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,32 +4,68 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
     //register user
     public function register(Request $request)
     {
-        // validate user input 
-        $feilds = $request->validate([
+        $fields = $request->validate([
             'name' => 'required|string',
             'email' => 'required|string|unique:users,email',
             'password' => 'required|string|confirmed'
         ]);
-        // create user 
-        $user = User::crete([
-            'name' => $feilds['name'],
-            'email' => $feilds['email'],
-            'password' => bcrypt($feilds['password'])
+
+        $user = User::create([
+            'name' => $fields['name'],
+            'email' => $fields['email'],
+            'password' => bcrypt($fields['password'])
         ]);
-        // genarate Token
+
         $token = $user->createToken('myapptoken')->plainTextToken;
-        
+
         $response = [
             'user' => $user,
             'token' => $token
         ];
-        // send response
-        return response($response,201);
+
+        return response($response, 201);
+    }
+
+    public function login(Request $request)
+    {
+        $fields = $request->validate([
+            'email' => 'required|string',
+            'password' => 'required|string'
+        ]);
+
+        // Check email
+        $user = User::where('email', $fields['email'])->first();
+
+        // Check password
+        if (!$user || !Hash::check($fields['password'], $user->password)) {
+            return response([
+                'message' => 'Bad creds'
+            ], 401);
+        }
+
+        $token = $user->createToken('myapptoken')->plainTextToken;
+
+        $response = [
+            'user' => $user,
+            'token' => $token
+        ];
+
+        return response($response, 200);
+    }
+
+    public function logout(Request $request)
+    {
+        auth()->user()->tokens()->delete();
+        return [
+            'message' => 'Logged out'
+        ];
     }
 };
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,15 +14,15 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
+// Public Routes
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
 // Protected Routes 
 Route::group(['middleware' => ['auth:sanctum']], function () {
-
+    Route::post('/logout', [AuthController::class, 'logout']);
     Route::get('/data', function () {
         return 'user data';
     });
-
-
 });
 
 Route::get('/hello', function () {
